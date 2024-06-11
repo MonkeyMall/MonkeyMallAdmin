b66521bc1dd24927f8afe8ac57d22cfae5602940
songyanbin <598349146>
2024-06-11 21:00:43 +0800

    提交登录

--- src/permission.js
+++ src/permission.js
@@ -22,31 +22,32 @@ function filterRoutesIcon(list1, list2) {
  */
 router.beforeEach(async(to, from, next) => {
 	// 存在 token ，进入主页
-	if (store.getters.token) { // 当前存在token，如果此时去登录界面，自动跳转到主页
-		if (to.path === "/login") {
-			next("/");
-		} else { // 如果此时，去除了登录页面的其他页面，就去要去的目标页面
-			if (!store.getters.hasRoles) {
-				const { roles } = await store.dispatch("user/getPermissionData");
-				// 处理用户权限，筛选出需要添加的权限
-				const accessRoutes = await store.dispatch("permission/generateRoutes", roles);
-				console.log("筛选出需要addRoute的路由", accessRoutes);
-				// 将左侧菜单的icon改为服务端数据
-				filterRoutesIcon(accessRoutes, roles);
-				// 利用 addRoute 循环添加
-				accessRoutes.forEach(item => {
-					router.addRoute(item);
-				});
-				next({ ...to, replace: true });
-			}
-			next();
-		}
-	} else {
-		// 没有token的情况下，可以进入白名单（不需要登录的界面）
-		if (whiteList.indexOf(to.path) > -1) {
-			next();
-		} else { // ，如果是需要登录的界面，去登录界面
-			next("/login");
-		}
-	}
+  next();
+	// if (store.getters.token) { // 当前存在token，如果此时去登录界面，自动跳转到主页
+	// 	if (to.path === "/login") {
+	// 		next("/");
+	// 	} else { // 如果此时，去除了登录页面的其他页面，就去要去的目标页面
+	// 		if (!store.getters.hasRoles) {
+	// 			const { roles } = await store.dispatch("user/getPermissionData");
+	// 			// 处理用户权限，筛选出需要添加的权限
+	// 			const accessRoutes = await store.dispatch("permission/generateRoutes", roles);
+	// 			console.log("筛选出需要addRoute的路由", accessRoutes);
+	// 			// 将左侧菜单的icon改为服务端数据
+	// 			filterRoutesIcon(accessRoutes, roles);
+	// 			// 利用 addRoute 循环添加
+	// 			accessRoutes.forEach(item => {
+	// 				router.addRoute(item);
+	// 			});
+	// 			next({ ...to, replace: true });
+	// 		}
+	// 		next();
+	// 	}
+	// } else {
+	// 	// 没有token的情况下，可以进入白名单（不需要登录的界面）
+	// 	if (whiteList.indexOf(to.path) > -1) {
+	// 		next();
+	// 	} else { // ，如果是需要登录的界面，去登录界面
+	// 		next("/login");
+	// 	}
+	// }
 });
