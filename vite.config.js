e36e136d597d4891bff4efa1839cdff86f436386
songyanbin <598349146>
2024-06-27 18:05:54 +0800

    接口地址的修改

--- vite.config.js
+++ vite.config.js
@@ -43,7 +43,7 @@ export default defineConfig({
 			"/api": {
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, ""),
-				// target: "http://127.0.0.1:9999/"
+        // target: "http://47.121.121.204:8080/api/"
 				target: "http://localhost:8080/api/"
 
 			}
